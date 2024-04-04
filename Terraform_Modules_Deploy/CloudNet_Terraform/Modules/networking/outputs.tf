6fc70ed423ad9265eec9df1092eec5a0f3de8117
NelieTchat <tchanela@gmail.com>
2024-04-03 22:38:21 -0700

    creating modules

--- Terraform_Modules_Deploy/CloudNet_Terraform/Modules/networking/outputs.tf
+++ Terraform_Modules_Deploy/CloudNet_Terraform/Modules/networking/outputs.tf
@@ -1,32 +1,41 @@
-# modules/outputs
+# modules/network/outputs.tf
+
 output "vpc_id" {
   value = aws_vpc.vpc.id
 }
 
-output "public_subnet1_id" {
-  value = aws_subnet.public_subnet1.id
+output "public_subnet_ids" {
+  value = concat(
+    [for s in aws_subnet.terra_pub_sub1 : s.id],
+    [for s in aws_subnet.terra_pub_sub2 : s.id]
+  )
 }
 
-output "public_subnet2_id" {
-  value = aws_subnet.public_subnet2.id
+output "private_subnet_ids" {
+  value = concat(
+    [for s in aws_subnet.terra_priv_sub1 : s.id],
+    [for s in aws_subnet.terra_priv_sub2 : s.id],
+    [for s in aws_subnet.terra_priv_sub3 : s.id],
+    [for s in aws_subnet.terra_priv_sub4 : s.id]
+  )
 }
 
-output "private_subnet1_id" {
-  value = aws_subnet.private_subnet1.id
+output "list_of_az" {
+  value = data.aws_availability_zones.available[*].names
 }
 
-output "private_subnet2_id" {
-  value = aws_subnet.private_subnet2.id
+output "internet_gateway_id" {
+  value = aws_internet_gateway.terra_igw.id
 }
 
-output "private_subnet3_id" {
-  value = aws_subnet.private_subnet3.id
+output "nat_gateway_id" {
+  value = aws_nat_gateway.nat_gateway.id
 }
 
-output "private_subnet4_id" {
-  value = aws_subnet.private_subnet4.id
+output "public_route_table_id" {
+  value = aws_route_table.pub_terra_rt.id
 }
 
-output "nat_gateway_public_ip" {
-  value = aws_nat_gateway.nat_gateway.public_ip
+output "private_route_table_id" {
+  value = aws_route_table.priv_terra_rt.id
 }
--- Terraform_Modules_Deploy/CloudNet_Terraform/install_apache.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-yum update -y        
-yum install -y httpd
-sudo systemctl start httpd.service
-echo " Nelie Launched her Apache Application through Terraform " > /var/www/html/index.html 
-sudo systemctl enable httpd.service
-sudo systemctl is-enabled httpd
-
